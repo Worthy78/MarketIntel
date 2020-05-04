612c6f3bc1082f48aa3080d46cea37a667c47468
akdiop <abdoukarim.diop1@orange-sonatel.com>
2020-05-04 15:03:03 +0000

    integration is up

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -3,10 +3,28 @@ import { NgModule } from '@angular/core';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
+import { HeaderComponent } from './header/header.component';
+import { NavbarComponent } from './navbar/navbar.component';
+import { SidebarComponent } from './sidebar/sidebar.component';
+import { GraphicComponent } from './layout/graphic/graphic.component';
+import { TableComponent } from './layout/table/table.component';
+import { MapsComponent } from './layout/maps/maps.component';
+import { ArticleComponent } from './layout/article/article.component';
+import { DetailComponent } from './layout/detail/detail.component';
+import { FooterComponent } from './footer/footer.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    HeaderComponent,
+    NavbarComponent,
+    SidebarComponent,
+    GraphicComponent,
+    TableComponent,
+    MapsComponent,
+    ArticleComponent,
+    DetailComponent,
+    FooterComponent
   ],
   imports: [
     BrowserModule,
